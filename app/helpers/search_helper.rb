3f80fc87fdb68320e6eae75c96804c7fa10dcaa0
Luca Bonmassar <luca.bonmassar@gmail.com>
2013-11-01 23:26:19 +0100

    make the 'found' surrounded by an EM tag

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,7 +1,7 @@
 module SearchHelper
 
   def total_found(gems)
-    (gems.respond_to? :total) ? "Found #{gems.total} results, showing #{gems.count}" : "Found #{gems.count} results."
+    (gems.respond_to? :total) ? "Found <em>#{gems.total}</em> results, showing <em>#{gems.count}</em>".html_safe : "Found <em>#{gems.count}</em> results.".html_safe
   end
   
 end
