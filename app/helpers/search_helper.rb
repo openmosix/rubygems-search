cca8abcd355d7a9d1faf386ae0b0c4e9578a9a0c
Luca Bonmassar <luca.bonmassar@gmail.com>
2013-11-02 01:47:13 +0100

    helper method to manipulate the original parameters

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -4,4 +4,17 @@ module SearchHelper
     (gems.respond_to? :total) ? "Found <em>#{gems.total}</em> results, showing <em>#{gems.count}</em>".html_safe : "Found <em>#{gems.count}</em> results.".html_safe
   end
   
+  def replace_search_param(original_params, name, value)
+    local_params = original_params.dup
+    local_params[:search] ||= {}
+    local_params[:search] = local_params[:search].dup
+    local_params[:search][name] = value
+    
+    local_params
+  end
+  
+  def original_search(params)
+    params[:search] && params[:search][:q]
+  end
+  
 end
